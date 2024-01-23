0598ce491155d94286c69187de30d04536fd2235
Vojtech Navratil <navryvoj@gmail.com>
2024-01-23 14:17:46 +0100

    model added

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class MyModel(models.Model):
+    percentage = models.IntegerField()
+    count_pool = models.IntegerField()
+    count_aquapark = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
