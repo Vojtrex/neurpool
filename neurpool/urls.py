3f53569f93c674499ebae2b57d02228e2494165d
Vojtech Navratil <navryvoj@gmail.com>
2024-01-23 14:00:27 +0100

    scraper added

--- neurpool/urls.py
+++ neurpool/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import JsonResponse
 
 
@@ -24,5 +24,6 @@ def index(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('scrape', include('scraper.urls'))
 ]
--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+import requests
+from bs4 import BeautifulSoup
+
+URL_SUTKA = 'https://www.sutka.eu/50m-plavecky-bazen'
+
+page = requests.get(URL_SUTKA)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+def scrape_url(request):
+
+    numbers = [strong.get_text(strip=True) for strong in soup.select('div.header-info strong')]
+    print(numbers)
+
+    f = open('output.html','w', encoding="utf-8")
+    f.write(page.text)
+
+    return HttpResponse(numbers)
+
+# print(page.text)
